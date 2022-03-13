76c1412da404c2bfbd67b84d69a9d646a7555561
xichuanwushao <avatarwx@163.com>
2022-03-13 14:00:55 +0800

    1.初始化项目

---
+++ src/main/java/com/xichuan/film/MeetingFilmApplication.java
@@ -0,0 +1,13 @@
+package com.xichuan.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingFilmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xichuan/film/MeetingFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xichuan.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
